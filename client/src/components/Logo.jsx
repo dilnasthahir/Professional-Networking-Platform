60808ab7d349b176d23dba7b2e58af694e3615b9
DILNAS THAHIR <dilnasthahir@gmail.com>
2024-07-04 00:20:32 +0530

    second commit

---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <div className='logo-main'>
+        <div className="logo-main-a">Connect</div><div className='logo-main-b'>in</div>
+    </div>
+  )
+}
+
+export default Logo
